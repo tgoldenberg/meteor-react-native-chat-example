ded55653f515a38429768eca66a904ee81e35d6f
tgoldenberg <thomasagoldenberg@gmail.com>
2015-11-16 12:37:19 -0500

    change app delegate

--- MeteorChatRN/app/config/ddp.js
+++ MeteorChatRN/app/config/ddp.js
@@ -5,7 +5,7 @@ import _ from 'underscore';
 
 let ddpClient = new DDPClient({
   // All properties optional, defaults shown
-  host : "192.168.1.137",
+  host : "192.168.1.3",
   port : 3000,
   ssl  : false,
   autoReconnect : true,
